4d09c954a551c95e7e94be4e823f9526ce72c455
ravikumark1150 <ravikumark11509@outlook.com>
2024-07-20 20:11:42 +0530

    add one book

--- Enum/src/enum1/Books.java
+++ Enum/src/enum1/Books.java
@@ -2,7 +2,7 @@ package enum1;
 
 public enum Books {
 
-	Corejava(45.33F), AdvJava(66.77F), spring(999.33F),oracle(588.55f);
+	Corejava(45.33F), AdvJava(66.77F), spring(999.33F),oracle(588.55f),spring2(222.33f);
 
 	public float price;
 
